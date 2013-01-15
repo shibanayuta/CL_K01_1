70c321b86662953bdc03c3b15e6806be7c4d8f84
shibanayuta <favorist50@gmail.com>
2013-01-15 10:04:00 +0900

    応用プログラミングAの課題１

---
+++ CL_K01_1/CL_K01_1.c
@@ -0,0 +1,12 @@
+/* ì¬Ò:1T5(23) Ä“ß—R‘¾ */
+/* ‰Û‘è–¼:‰Û‘è01_1 */
+
+#include <stdio.h>
+
+void main(void) {
+	int sum,num1,num2;
+	num1 = 15;
+	num2 = 37;
+	sum =num1+num2;
+	printf("%d‚É%d‚ğ‘«‚µ‚½’l‚Í%d‚Å‚·\n", num1,num2,sum);
+}
